86162b7dfcd9d5946eb9a02d6c05b25042b1f762
SuriChen1028 <shirui.chan@gmail.com>
2025-12-24 23:42:48 -0600

    first commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "FiniteDifferenceSC.jl" begin
     # Write your tests here.
+    @test greet_your_package_name() == "Hello FiniteDifferenceSC!"
+    @test greet_your_package_name() != "Hello YourPackageName!"
 end
